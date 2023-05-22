5f08cc8ffc456c04ca2c5927d249b53b203fd6c3
tklim99 <devtklim99@gmail.com>
2023-05-22 18:22:05 +0900

    Update SteppingMotor.ino

--- SteppingMotor/SteppingMotor.ino
+++ SteppingMotor/SteppingMotor.ino
@@ -1,28 +1,28 @@
 #include <AFMotor.h>
 
 AF_Stepper motor(20,2);
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("Stepper test");
-
   motor.setSpeed(200);
 }
 
 void loop() {
-  Serial.println("Single coil steps");
-  motor.step(200, FORWARD, SINGLE);
-  motor.step(200, BACKWARD, SINGLE);
 
-  Serial.println("Double coil steps");
-  motor.step(200, FORWARD, DOUBLE);
-  motor.step(200, BACKWARD, DOUBLE);
-
-  Serial.println("Interleave coil steps");
-  motor.step(200, FORWARD, INTERLEAVE);
-  motor.step(200, BACKWARD, INTERLEAVE);
-
-  Serial.println("Micrsostep steps");
-  motor.step(200, FORWARD, MICROSTEP);
-  motor.step(200, BACKWARD, MICROSTEP);
-
+  if(Serial.available()>0)
+  {
+    char CoP = Serial.read();
+    if (CoP == 'c')
+    {
+      Serial.println("Can");
+      motor.step(360, FORWARD, SINGLE);
+      while (Serial.read() == 'c');
+    }
+    else if (CoP == 'p')
+    {
+      Serial.println("Plastic");
+      motor.step(360,BACKWARD,SINGLE);
+      while (Serial.read() == 'p');
+    }
+  }
 }
